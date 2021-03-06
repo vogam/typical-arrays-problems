1d32bd58672fe314208276c9ef3cdba08ffe88e6
Vogam <dbelov796@gmail.com>
2021-03-06 19:48:18 +0300

    feat: add fun min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if  ( typeof(array) === 'undefined' || array.length === 0) return 0;
+    let result = Infinity;
+    for(let i = 0; i<array.length; i++){
+        if (array[i] < result){
+        result = array[i]
+    }
+        }
+     return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+   if  ( typeof(array) === 'undefined' || array.length === 0) return 0;
+    let result = -Infinity;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] > result)
+            {
+                result = array[i]
+            }
+    }
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   if  ( typeof(array) === 'undefined' || array.length === 0) return 0;
+    let result = 0;
+    for(let i = 0; i < array.length; i++){
+        result += array[i]
+    }
+    return (result/ array.length).toFixed(2);
 }
